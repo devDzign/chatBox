a59027f4d32bf9a643cddc9f1bcff4ee47934a77
mchabour <mchabour@largus.fr>
2017-04-01 23:26:57 +0200

    module conmpteur message

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,26 @@ import Message from './Message';
 import Formulaire from './Formulaire';
 
 class App extends React.Component {
+
+    state = {
+        messages:{}
+    }
+
+
+    addMessage = (message) => {
+
+        //Copie le state
+        const messages = {...this.state.messages};
+
+        //On ajoute le message avec une cle timstamp
+        const timestamp = Date.now();
+        messages[`message-${timestamp}`] = message;
+
+        //mettre a jour le state
+        this.setState({messages});
+    };
+
+
 	render() {
 		return (
 			<div className="box">
@@ -10,7 +30,11 @@ class App extends React.Component {
 					<div className="messages" >
 						<Message pseudo={this.props.params.pseudo} />
 					</div>
-					<Formulaire />
+					<Formulaire
+                        addMessage={this.addMessage}
+                        pseudo={this.props.params.pseudo}
+                        length="140"
+                    />
 				</div>
 			</div>
 		)
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,15 +1,52 @@
 import React from 'react';
 
 class Formulaire extends React.Component {
+
+	state ={
+        length: this.props.length
+	}
+
+	createMessage = event => {
+	   event.preventDefault();
+		const message = {
+			message : this.message.value,
+			pseudo : this.props.pseudo
+		};
+
+		this.props.addMessage(message);
+
+		//Reset form
+		this.messageForm.reset();
+
+		//Mettre la lengueur à jour 140
+		this.setState({length: this.props.length});
+	};
+
+    compteur = event => {
+        event.preventDefault();
+
+        const length = this.props.length - this.message.value.length;
+		this.setState({length});
+    };
+
 	render() {
 		return (
-			<form className="form">
+			<form
+				className="form"
+				onSubmit={e => this.createMessage(e)}
+				ref={input =>  this.messageForm = input}
+				onChange={ e => this.compteur(e)}
+			>
 
-				<textarea required maxLength="140" >
+				<textarea
+					required
+					maxLength={this.props.length}
+					ref={input => this.message = input}
+				>
 				</textarea>
 
 				<div className="info" >
-						140
+					{this.state.length}
 				</div>
 
 				<button type="submit" >
